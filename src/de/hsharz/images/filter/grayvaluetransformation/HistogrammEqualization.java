99b62fcd4f72bc1bed5add96ee35790d8de9646b
Oliver Lindemann <support@spiderlinker.de>
2020-11-17 15:51:11 +0100

    Added HistogramEqualization

--- src/de/hsharz/images/filter/grayvaluetransformation/HistogrammEqualization.java
+++ src/de/hsharz/images/filter/grayvaluetransformation/HistogrammEqualization.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 
 import de.hsharz.images.filter.AbstractFilter;
 import de.hsharz.images.filter.utils.ImageUtils;
-import de.hsharz.images.ui.ImageInfo;
 import de.hsharz.images.ui.ImageInfo.ImageColor;
 
 public class HistogrammEqualization extends AbstractFilter {
@@ -15,36 +14,53 @@ public class HistogrammEqualization extends AbstractFilter {
 		this.color = color;
 	}
 
-	@Override
-	public ImageInfo perform(ImageInfo imageInfo) {
-		// Create a copy of the input image, transform it and return as result
-		BufferedImage inputImage = imageInfo.getImage();
-		BufferedImage outputImage = ImageUtils.getCopyOf(imageInfo.getImage());
-
-		int[] adjustedValues = new int[imageInfo.getColorValueCount(color).length];
-
-		int sum = 0;
-		for (int i = 0; i < 255; i++) {
-			sum += imageInfo.getColorValueCount(color)[i];
-			adjustedValues[i] = sum * 255 / imageInfo.getPixelCount();
+	private int insertRgbInOriginalRgb(int origRgb, int rgb) {
+		int mask;
+		int shift;
+		switch (color) {
+		case RED:
+			mask = 0xff00ffff;
+			shift = 16;
+			break;
+		case GREEN:
+			mask = 0xffff00ff;
+			shift = 8;
+			break;
+		case BLUE:
+			mask = 0xffffff00;
+			shift = 0;
+			break;
+		default:
+			mask = 1;
+			shift = 0;
 		}
-
-		// tranform every pixel to gray
-		for (int x = 0; x < inputImage.getWidth(); x++) {
-			for (int y = 0; y < inputImage.getHeight(); y++) {
-
-				// Update current Pixel to created gray-pixel
-				outputImage.setRGB(x, y,
-						adjustedValues[ImageUtils.getColorValueOfPixel(color, inputImage.getRGB(x, y))]);
-
-			}
-		}
-		return new ImageInfo(imageInfo.getImageFile(), outputImage);
+		return (origRgb & mask) | (rgb << shift);
 	}
 
 	@Override
 	public void transformOutputImage(BufferedImage inputImage) {
-		// all work already done in perform method
+		int result;
+		float newPixelVal;
+
+		int currentPixelVal;
+
+		// Now calculate the new intensity values
+		for (int x = 0; x < inputImage.getWidth(); x++) {
+			for (int y = 0; y < inputImage.getHeight(); y++) {
+				currentPixelVal = ImageUtils.getColorValueOfPixel(color, inputImage.getRGB(x, y));
+				newPixelVal = 0;
+				for (int j = 0; j < (currentPixelVal + 1); j++) {
+					newPixelVal += getImageInfo().getColorValueCount(color)[j]
+							/ (double) getImageInfo().getPixelCount();
+				}
+				result = (int) (newPixelVal * 255);
+
+				int origRgb = inputImage.getRGB(x, y);
+				int newRgb = insertRgbInOriginalRgb(origRgb, result);
+
+				getOutputImage().setRGB(x, y, newRgb);
+			}
+		}
 	}
 
 }
---
+++ src/de/hsharz/images/ui/filter/HistogramEqualizationPane.java
@@ -0,0 +1,116 @@
+package de.hsharz.images.ui.filter;
+
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+
+import de.hsharz.images.filter.grayvaluetransformation.HistogrammEqualization;
+import de.hsharz.images.filter.utils.ImageUtils;
+import de.hsharz.images.ui.ImageInfo;
+import de.hsharz.images.ui.ImageInfo.ImageColor;
+import de.hsharz.images.utils.LayoutUtils;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+public class HistogramEqualizationPane {
+
+	private GridPane root;
+	private ImageView previewImg;
+	private Button btnChoose;
+
+	private HBox boxButtons;
+//	private RadioButton btnGray;
+	private RadioButton btnRed;
+	private RadioButton btnGreen;
+	private RadioButton btnBlue;
+
+	private ImageColor currentVal;
+
+	private ObjectProperty<Image> originalImage = new SimpleObjectProperty<>();
+	private BufferedImage originalBufferedImage;
+
+	public HistogramEqualizationPane(ImageInfo image) {
+		Objects.requireNonNull(image);
+		this.originalBufferedImage = ImageUtils.resize(image.getImage(), 300, 200);
+		this.originalImage.set(SwingFXUtils.toFXImage(originalBufferedImage, null));
+
+		createWidgets();
+		setupInteractions();
+		addWidgets();
+	}
+
+	private void createWidgets() {
+		root = new GridPane();
+		root.setPadding(new Insets(10));
+		root.setHgap(20);
+		root.setVgap(20);
+		LayoutUtils.setColumnWidths(root, 50, 50);
+		LayoutUtils.setRowHeight(root, 80, 10, 10);
+
+		boxButtons = new HBox(20);
+		boxButtons.setMaxWidth(Double.MAX_VALUE);
+//		boxButtons.setAlignment(Pos.CENTER_LEFT);
+
+//		btnGray = new RadioButton("Grau");
+		btnRed = new RadioButton("Rot");
+		btnGreen = new RadioButton("Grün");
+		btnBlue = new RadioButton("Blau");
+
+		ToggleGroup group = new ToggleGroup();
+//		group.getToggles().add(btnGray);
+		group.getToggles().add(btnRed);
+		group.getToggles().add(btnGreen);
+		group.getToggles().add(btnBlue);
+
+		previewImg = new ImageView();
+		previewImg.imageProperty().bind(originalImage);
+
+		btnChoose = new Button("Übernehmen");
+		btnChoose.setDisable(true);
+	}
+
+	private void setupInteractions() {
+//		btnGray.setOnAction(e -> calculateNewImage(ImageColor.GRAY));
+		btnRed.setOnAction(e -> calculateNewImage(ImageColor.RED));
+		btnGreen.setOnAction(e -> calculateNewImage(ImageColor.GREEN));
+		btnBlue.setOnAction(e -> calculateNewImage(ImageColor.BLUE));
+	}
+
+	private void calculateNewImage(ImageColor val) {
+		currentVal = val;
+		btnChoose.setDisable(false);
+		BufferedImage newImage = new HistogrammEqualization(val).perform(new ImageInfo(originalBufferedImage))
+				.getImage();
+		originalImage.set(SwingFXUtils.toFXImage(newImage, null));
+	}
+
+	private void addWidgets() {
+		boxButtons.getChildren().addAll(btnRed, btnGreen, btnBlue);
+		
+		root.add(previewImg, 0, 0, 2, 1);
+		root.add(boxButtons, 0, 1, 2, 1);
+		root.add(btnChoose, 1, 2);
+	}
+
+	public Button getButtonChoose() {
+		return btnChoose;
+	}
+
+	public ImageColor getSelectedValue() {
+		return currentVal;
+	}
+
+	public Pane getPane() {
+		return root;
+	}
+
+}
